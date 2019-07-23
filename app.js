7f1d6f748910ebcb38e8774a5a94cb572eecc254
Dany <nxs.dany@gmail.com>
2019-07-23 22:18:20 +0400

    добавлена поддержка бд sqlite

--- app.js
+++ app.js
@@ -41,6 +41,25 @@ app.use(function(err, req, res, next) {
 
 module.exports = app;
 
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('test.db');
+
+db.serialize(function () {
+  db.run("CREATE TABLE lorem (info TEXT)");
+
+  var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
+  for (var i = 0; i < 10; i++) { 
+    stmt.run("Ipsum " + i);
+  }
+  stmt.finalize();
+
+  db.each("SELECT rowid AS id, info FROM lorem", function (err, row) {
+    console.log(row.id + ": " + row.info);
+  });
+});
+
+db.close();
+
 app.listen(3000, function () {
   console.log('This app listening on port 3000!');
 });
