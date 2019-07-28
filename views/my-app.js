59d8a77943cf4786f64124453ca6750dc16b709d
Sergey <sergejsivk@mail.ru>
2019-07-28 23:09:30 +0400

    Поправил аякс создания записи, сделал, как у боота в телеге

--- views/my-app.js
+++ views/my-app.js
@@ -11,28 +11,53 @@ new Vue({
 	},
 	methods: {
 		addTask:function(){
-			axios.post('/app.js',{
-				methods: 'addTask',
-				task: event.target.value
-			})
+			var value=event.target.value;
+			if (value)
+				this.ajax({message:{text:value}})
+		},
+		markTask:function(){
+			this.$f7.dialog.alert('task '+this.getTask(event)+' mark');
+		},
+		sendArc:function(){
+			this.ajax('sendArc',this.getTask(event))
+			this.$f7.dialog.alert('task '+this.getTask(event)+' send arc');
+		},
+		taskEdit:function(){
+			const self = this;
+			if (!self.popup) {
+				self.popup = self.$f7.popup.create({
+					content: `
+						<div class="popup">
+							<div class="page">
+								<div class="navbar">
+									<div class="navbar-inner">
+										<div class="title">Редактирование задачи</div>
+										<div class="right"><a href="#" class="link popup-close">Отмена</a></div>
+									</div>
+								</div>
+								<div class="page-content">
+									<div class="block">
+										<p>заготовачка для шаблончика с бека</p>
+									</div>
+								</div>
+							</div>
+						</div>
+					`.trim(),
+				});
+				}
+				self.popup.open();
+			},
+		getTask:function(a){
+			return a.target.parentElement.parentElement.getAttribute('task');
+		},
+		ajax:function(params){
+			axios.post('/app.js',params)
 			.then(function (response) {
 				console.log(response);
 			})
 			.catch(function (error) {
 				console.log(error);
 			});
-		},
-		taskEdit:function(){
-			this.$f7.dialog.alert('task '+this.getTask(event)+' edit');
-		},
-		markTask:function(){
-			this.$f7.dialog.alert('task '+this.getTask(event)+' mark');
-		},
-		sendArc:function(){
-			this.$f7.dialog.alert('task '+this.getTask(event)+' send arc');
-		},
-		getTask:function(a){
-			return a.target.parentElement.parentElement.getAttribute('task');
 		}
 	}
 })
